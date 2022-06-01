28c75b70cd9dc7faef8658dc12203ab66cea3ff3
jhayash1 <yash.jha16@gmail.com>
2022-06-01 16:44:17 +0530

    Updates

---
+++ precache-manifest.a7cf5d3052cadb6c1dd0501ff5ed6e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026e440dd6d4de4c8eb3d2240abc4f6f",
+    "url": "/Text-utilites/index.html"
+  },
+  {
+    "revision": "7fa20e02997bb71e6d2a",
+    "url": "/Text-utilites/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f06fae0703c15a0f09c1",
+    "url": "/Text-utilites/static/js/2.421402e3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Text-utilites/static/js/2.421402e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa20e02997bb71e6d2a",
+    "url": "/Text-utilites/static/js/main.5c08eafb.chunk.js"
+  },
+  {
+    "revision": "735c7249836d2444bc08",
+    "url": "/Text-utilites/static/js/runtime-main.463746de.js"
+  }
+]);
